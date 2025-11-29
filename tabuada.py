803928f87b3d63a2df3ed3f05af9f660a2e9aa80
Izaquielsantos <isaquiels83@gmail.com>
2025-11-28 23:27:21 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+while True:
+    nn= int(input('que ver a tabuadar de qual valor please?'))
+    if nn <0:
+        break
+    for y in range(1,11):
+        print (f'{nn} x {y} = {nn*y}')
+print (f' OBRIGADO PRO USAR O MEU PROGRAMA FOI LA ELI ')
+
