afd974ea2dd89edadf64b3ce123cadd0c4b6fe63
Izaquielsantos <isaquiels83@gmail.com>
2025-12-03 12:30:06 -0300

    pedra papel tesoura

--- dia-2-pedra-papel-tesoura/pedra-papel-tesoura.py
+++ dia-2-pedra-papel-tesoura/pedra-papel-tesoura.py
@@ -3,4 +3,4 @@ g2=int(input('qual e sua idade por favor?'))
 g3=str(input('ne qual cidade voce mora por favor ?'))
 g4=int(input('qual e sua media salarial ?'))
 
-print (f'oi tudo bem, {gg} com voce vai . voce mora em {g3} e sua idade e {g2}a sua media salarial o que voce quee ganha e  {g4}')
+print (f'oi tudo bem, {gg} com voce vai . voce mora em {g3} e sua idade e {g2}a sua media salarial o que voce quee ganha ee  {g4}')
