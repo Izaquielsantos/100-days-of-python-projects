2be3d0828cc9f1556332c1bbaf726ea38519bb55
Izaquielsantos <isaquiels83@gmail.com>
2025-12-08 11:11:16 -0300

    EMPRÉSTIMO

---
+++ dia-1-apravador-de-emprestimo-python/aprovador-de-emprestimo.py
@@ -0,0 +1,11 @@
+casa= float(input('qual e valo da casa por favor'))
+salario=float(input('qual e valo do seu salario por favo'))
+anos=int(input('EM quantos anos voce vai paga'))
+prestacao= casa/(anos*12)
+pos=salario*30/100
+print(f'para pagar a casa de {casa} ')
+print(f'A sua prestacao foi de {prestacao}')
+if prestacao<=pos:
+    print ('O seu emprestimo foi aprovado , agora e so curti o seu emprestimo')
+else:
+    print ('min descupe o seu Emprestimo foi recusado, nao deu volte sempre ')
